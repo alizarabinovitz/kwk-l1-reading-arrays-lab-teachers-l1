a4eb1f624b2e15c6a1eb34e4a8bfb5fca552c855
Aliza Rabinovitz <alizajessa24@gmail.com>
2018-06-20 00:45:46 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -20,7 +20,7 @@ def last_student_by_index
 end
 
 def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+  return student_names.first# Write a solution that returns the first student using the built in .first method
 end
 
 def last_student_by_method
